faa47b3f20b74ccad1f3686f31dc48d5f31974e6
Bahaeddine Dayya <161286313+fluffychilidogz@users.noreply.github.com>
2026-02-04 20:28:34 +0000

    finished task2

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,7 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+
+SELECT DepartmentName, COUNT(StudentId)
+FROM Student, Department
+GROUP BY DepartmentName;
--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,3 +1,8 @@
 -- For each student, calculate the total number of credits from courses they passed. Assume a passing grade is 40 or higher.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
+
+SELECT Student.StudentId, FirstName, LastName, SUM(Credits) as TotalCreditsPassed 
+FROM Student JOIN Enrolment ON Student.StudentId=Enrolment.StudentId
+JOIN Course ON Enrolment.CourseId=Course.CourseId
+WHERE Enrolment.grade >= 40 Group by Student.StudentId, FirstName, LastName;
